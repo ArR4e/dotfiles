1099a6e1a616c337b67e9083e82489b76a7fda8e
Artur Aleksander Kanošin <81518066+ArR4e@users.noreply.github.com>
2022-09-08 10:48:37 +0300

    first commit

---
+++ installs.sh
@@ -0,0 +1,16 @@
+sudo apt install tmux
+apt install ./nvim-linux64.deb
+#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+#curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#mkdir ~/.config/nvim/vim-plug
+#cd  ~/.config/nvim/vim-plug
+#touch ~/.config/nvim/vim-plug/plugins.vim
+
+
+apt install npm
+apt-get install -y nodejs
+apt-get install fzf
+apt install zsh && sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+apt install tldr
---
+++ installszsh.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions\
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+apt install bat
